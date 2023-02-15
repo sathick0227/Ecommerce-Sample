9029f6b120a2ab93284cae747f96c4d46563f7ab
sathick0227 <sathickbatcha71@gmail.com>
2023-02-15 16:24:56 +0400

    first Sample

---
+++ src/Home.js
@@ -0,0 +1,20 @@
+import FeatureProduct from "./components/FeatureProduct";
+import HeroSection from "./components/HeroSection";
+// import Services from "./components/Services";
+// import Trusted from "./components/Trusted";
+
+const Home = () => {
+  const data = {
+    name: "thapa store",
+  };
+
+  return (
+    <>
+      <HeroSection myData={data} />
+      <FeatureProduct />
+      
+    </>
+  );
+};
+
+export default Home;
